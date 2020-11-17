2ef0317186c5d30da5bf6e823054c5aadb52ed5a
Rostyslav Vyshemirskyi <vyshemirskyirostyslav@gmail.com>
2020-11-17 17:47:30 +0200

    #7 Blocks and templates: added uk_UA locale for the main website

--- dev/tools/grunt/configs/local-themes.js
+++ dev/tools/grunt/configs/local-themes.js
@@ -1,7 +1,18 @@
 /**
+ * grunt exec:rostyslav_luma_uk_ua && grunt less:rostyslav_luma_uk_ua && grunt watch
  * grunt exec:rostyslav_luma_en_us && grunt less:rostyslav_luma_en_us && grunt watch
  */
 module.exports = {
+    rostyslav_luma_uk_ua: {
+        area: 'frontend',
+        name: 'Rostyslav/luma',
+        locale: 'uk_UA',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
     rostyslav_luma_en_us: {
         area: 'frontend',
         name: 'Rostyslav/luma',
