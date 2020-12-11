629d4729facf4cb100257caba310a0ffd6e69244
Rostyslav Vyshemirskyi <vyshemirskyirostyslav@gmail.com>
2020-12-11 23:52:02 +0200

    #10 jQuery widgets: Added Rostyslav_RegularCustomer module which adds a new tab to the Product Page

--- app/etc/config.php
+++ app/etc/config.php
@@ -312,6 +312,7 @@ return [
         'PayPal_BraintreeGraphQl' => 1,
         'Rostyslav_Cms' => 1,
         'Rostyslav_ControllerDemo' => 1,
+        'Rostyslav_RegularCustomer' => 1,
         'Temando_ShippingRemover' => 1,
         'Vertex_Tax' => 1,
         'Vertex_AddressValidationApi' => 1,
